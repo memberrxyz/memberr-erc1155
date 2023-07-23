3ef75af435a41a62fc85f4a62ed0547d652371a9
Sören Steiger <soren1@gmx.net>
2023-07-23 14:21:10 +0200

    Add burn functionality

--- packages/hardhat/contracts/MemberrCollectible.sol
+++ packages/hardhat/contracts/MemberrCollectible.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 contract MemberrCollectible is 
     ERC1155,
     ERC1155Supply,
+    ERC1155Burnable,
+    Ownable,
     AccessControl
 {
     bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
