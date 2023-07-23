dd9ee69abea2d87c5b839ae93a8f3a785e138157
Sören Steiger <soren1@gmx.net>
2023-07-23 14:53:22 +0200

    Remove burn and pause functionality

--- packages/hardhat/contracts/MemberrCollectible.sol
+++ packages/hardhat/contracts/MemberrCollectible.sol
@@ -1,23 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract MemberrCollectible is 
     ERC1155,
     ERC1155Supply,
-    ERC1155Burnable,
-    Pausable,
-    Ownable,
     AccessControl
 {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     string private _contractUri;
 
     constructor(
@@ -85,13 +79,7 @@ contract MemberrCollectible is
         _mintBatch(to, ids, amounts, data);
     }
 
-    function pause() public onlyOwner {
-        _pause();
-    }
 
-    function unpause() public onlyOwner {
-        _unpause();
-    }
 
     // The following functions are overrides required by Solidity.
 
@@ -104,7 +92,6 @@ contract MemberrCollectible is
         bytes memory data
     )
         internal
-        whenNotPaused
         override(ERC1155, ERC1155Supply)
     {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
